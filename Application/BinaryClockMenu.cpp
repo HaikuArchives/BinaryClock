b72023a0714929493dd168444f3866fcb30899bd
Josef Gajdusek <atx@atx.name>
2014-12-23 13:00:01 +0100

    Application: Load resources from an .rdef file

--- Application/BinaryClockMenu.cpp
+++ Application/BinaryClockMenu.cpp
@@ -15,69 +15,34 @@
 #include "BinaryClockMenu.h"
 
 // Constructor
-TMainMenu::TMainMenu(char *name, BDirectory *graphicsDir, int tnumcolors) : BPopUpMenu(name, false, false, B_ITEMS_IN_COLUMN)
+TMainMenu::TMainMenu(char *name, const char* colorNames[]) : BPopUpMenu(name, false, false, B_ITEMS_IN_COLUMN)
 {
 	BMenuItem *tmpItem;
-	BMenuItem *DotColor[COLOR_ARRAY_SIZE];
 
 	printf("-1-MainMenu\n");
 
-	numcolors = tnumcolors;
-
-	int i = 0;	// counting variable
-	BDirectory *colorDir = new BDirectory();
-	BEntry *nextColorDir = new BEntry();
-	BPath *curPath = new BPath();
-	BMessage *tmpMsg;
-	char curDirStr[B_FILE_NAME_LENGTH];
-	graphicsDir->Rewind();
-	while (graphicsDir->GetNextEntry(nextColorDir) == B_OK) {
-		colorDir = new BDirectory(nextColorDir);
-		if ((colorDir->Contains("on", B_FILE_NODE)) && (colorDir->Contains("off", B_FILE_NODE))) {
-			if (nextColorDir->GetPath(curPath) == B_OK) {
-				sprintf(curDirStr, "%s", curPath->Leaf());
-				tmpMsg = new BMessage(CHNGCOLOR);
-				tmpMsg->AddInt16("newcolor", i);
-				DotColor[i] = new BMenuItem(curDirStr, tmpMsg);
-				i++;
-			}
-		}
+	for (int i = 0; colorNames[i] != NULL; i++) {
+		BMessage* msg = new BMessage(CHNGCOLOR);
+		msg->AddInt16("newcolor", i);
+		BMenuItem* item = new BMenuItem(colorNames[i], msg);
+		AddItem(item);
+		if (i == 0)
+			item->SetMarked(true);
 	}
 
-	if (i == numcolors) {
-		for (int a=0; a<numcolors; a++) {
-			AddItem(DotColor[a]);
-		}
+	AddSeparatorItem();
 
-		AddSeparatorItem();
-	
-		tmpMsg = new BMessage(CHNG12HR);
-		tmpItem = new BMenuItem("12 Hour", tmpMsg);
-		AddItem(tmpItem);
-		tmpMsg = new BMessage(CHNG24HR);
-		tmpItem = new BMenuItem("24 Hour", tmpMsg);
-		AddItem(tmpItem);
-	
-		printf("-3-MainMenu\n");
-	}
-	else {
-		BAlert *ExitAlert = new BAlert("Fatal Error", "The contents of the \"[user settings]/BinaryClock_2.xx/Graphics/\" directory changed during execution.", "I'm Sorry", NULL, NULL, B_WIDTH_AS_USUAL, B_STOP_ALERT);
-		ExitAlert->Go();
-		exit(1);
-	}
+	tmpItem = new BMenuItem("12 Hour", new BMessage(CHNG12HR));
+	AddItem(tmpItem);
+	tmpItem = new BMenuItem("24 Hour", new BMessage(CHNG24HR));
+	AddItem(tmpItem);
 
-	DotColor[0]->SetMarked(true);
+	printf("-3-MainMenu\n");
 }
 
 // CONSTRUCTOR from Archive
 TMainMenu::TMainMenu(BMessage *archive) : BPopUpMenu(archive)
 {
-	BMessage btemp;
-
-	// get number of colors archived
-	numcolors = archive->FindInt16("numcolors");
-	
-	printf("-MainMenu-CONSTRUCTOR from Archive - numcolors: %i\n", numcolors);
 }
 
 // Archive function
@@ -86,8 +51,6 @@ status_t TMainMenu::Archive(BMessage *archive, bool deep) const
 	if (BPopUpMenu::Archive(archive, deep) != B_OK) {
 		return B_ERROR;
 	}
-
-	archive->AddInt16("numcolors", numcolors);
 
 	return B_OK;
 }
